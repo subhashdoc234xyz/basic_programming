1a6f0e1c2e02e1176ee4f6a25159a058b81e3c00
subhashdoc234xyz <subhashdoc234@gmail.com>
2026-07-13 07:32:41 +0530

    new

--- python/Modular Code/08_functions/exfunction2.py
+++
@@ -1,5 +0,0 @@
-def hello(to ):#giving an parameter to the function
-    print("hello,",to)
-
-name=input("enter your name:")
-hello(name)# copies the value of name to the parameter to in the function hello
--- python/Modular Code/08_functions/exfunctionprog.py
+++
@@ -1,6 +0,0 @@
-def hello():
-    print("hello,")
-    
-name=input("Enter your name:" )
-hello()
-print(name)
